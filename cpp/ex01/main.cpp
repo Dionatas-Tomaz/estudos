a51b1cb454fbc59b09d823a6147ed3cab2936c26
dionatas-tomaz <dionatastomaz0@gmail.com>
2022-11-06 19:33:05 -0300

    edit

---
+++ cpp/ex01/main.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+int idade;
+
+float salario;
+
+char nome[50];
+
+char sobrenome[50];
+
+
+main (void){
+
+    setlocale(LC_ALL, "portuguese");
+ 
+    printf("digite sua idade:");
+    scanf("%i",&idade);
+
+    printf("seu salario:");
+    scanf("%f",&salario);
+
+    printf("seu nome:");
+    scanf("%s",&nome);
+
+    printf("sobrenome:");
+    scanf("%s",sobrenome);
+
+    printf("os dados informados são:%i %.2f %s %s\n",idade,salario,nome,sobrenome);
+    system("pause");
+    system("cls");
+}
+    
